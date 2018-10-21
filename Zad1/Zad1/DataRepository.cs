58619aafe185e0e1139640c0af217a114b3c0961
przemo <p.otrembski@wp.pl>
2018-10-21 19:06:21 +0200

    zrobilismy Czesc 1 podpunkt d)

---
+++ Zad1/Zad1/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class DataRepository
+    {
+        private DataContext dataContext;
+
+        public DataContext DataConst { set; private get; }
+
+    }
+}
