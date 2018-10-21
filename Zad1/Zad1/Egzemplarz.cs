49a676c3f1743662300ed00f2b56544889ab4587
przemo <p.otrembski@wp.pl>
2018-10-21 17:59:22 +0200

    dodalem klasy z diagramu ale trzeba zmienic troche aby bylo jak w tresci zadania

---
+++ Zad1/Zad1/Egzemplarz.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+
+    abstract class Egzemplarz
+    {
+        protected int id;
+        protected string tytul;
+        protected Boolean stan;
+        protected Boolean zarezerwowany;
+        protected Rodzaj rodzajEgz; // TODO
+        protected int licznikWypozyczen;
+
+        public int Id { get; set; }
+        public string Tytul { get; set; }
+        public Boolean Stan { get; set; }
+        public Boolean Zarezerwowany { get; set; }
+        //public RodzajEgz {get; set;}
+        public int LicznikWypozyczen { get; set; }
+
+        public Egzemplarz(int id, string tytul,Rodzaj rodzajEgz)
+        {
+            this.id = id;
+            this.tytul = tytul;
+            this.stan = true;
+            this.zarezerwowany = false;
+            this.rodzajEgz = rodzajEgz;
+            this.licznikWypozyczen = 0;
+        }
+
+        public override string ToString()
+        {
+            string ret = "Identyfikator: " + id + " Rodzaj: " + rodzajEgz;
+            if (stan)
+            {
+                ret += " Egzmplarz dostepny";
+            }
+            else {
+                ret += " Egzemplarz Niedostepny";
+            }
+
+            if (zarezerwowany) ret += " Egzemplarz ZAREZERWOWANY!";
+
+            ret += " Tytul: " + tytul;
+            return ret;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Egzemplarz)
+            {
+                var tempBook = (Egzemplarz)obj;
+                return id.Equals(tempBook.id) && tytul.Equals(tempBook.tytul) && licznikWypozyczen.Equals(tempBook.licznikWypozyczen);
+               
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
