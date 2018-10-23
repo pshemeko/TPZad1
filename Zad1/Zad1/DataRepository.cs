df80b387f4f0406a32910d6d43cf274a7ebb9acc
przemo <p.otrembski@wp.pl>
2018-10-23 23:22:15 +0200

    dodalismy klase DataService
    zrobiono Czesc 4 podpunkt f)

--- Zad1/Zad1/DataRepository.cs
+++ Zad1/Zad1/DataRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Zad1
 {
-    class DataRepository
+    public class DataRepository
     {
         private DataContext dataContext;
         private Wypelnianie wypelniacz;
