98458a8f2c7fa544b5f0a6ae7c7e56e8ae275653
ajozefowicz <ginger4u@op.pl>
2018-10-27 16:44:41 +0200

    dodalam testy

---
+++ Zad1/Zad1Tests/DataRepositoryTests.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zad1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1.Tests
+{
+    [TestClass()]
+    public class DataRepositoryTests
+    {
+        /*
+        [TestMethod()]
+        public void WypelnijTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddUzytkownikaTest()
+        {
+            Assert.Fail();
+        }
+        */
+        [TestMethod()]
+        public void AddEgzemplarzTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddZdarzenieWypozyczenieTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddZdarzenieZwrotTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddOpisStanuEgzemplarzaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUzytkownikaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEgzemplarzTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetZdarzenieTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOpisStanuEgzemplarzaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllUzytkownikowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllOpisStanuEgzemplarzasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateUzytkownikTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateEgzemplarzTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateZdarzenieTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateOpisStanowEgzemplarzaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteUzytkownikTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteEgzemplarzTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteZdarzenieTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteOpisStanuEgzemplarzaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
